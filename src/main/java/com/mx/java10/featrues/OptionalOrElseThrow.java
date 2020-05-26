5c54fc2b1b821aa3f4f56bb211e1ebccb223bfe0
seeand <kdsfoxy@gmail.com>
2020-05-26 17:35:24 -0500

    Optional Or Else Throw new Feature

--- src/main/java/com/mx/java10/featrues/OptionalOrElseThrow.java
+++ src/main/java/com/mx/java10/featrues/OptionalOrElseThrow.java
@@ -1,4 +1,21 @@
 package com.mx.java10.featrues;
 
+import java.util.List;
+
 public class OptionalOrElseThrow {
+
+    public static void main(String[] args) {
+
+        List<Integer> someIntList = List.of(1,2,3,4,5); //int type isn't inferred
+
+        Integer firstEven = someIntList.stream()
+                .filter(i -> i % 2 == 0)
+                .findFirst()
+                .orElseThrow();
+
+        if((firstEven).equals(Integer.valueOf(2))) {
+            System.out.println("firstEven is equals");
+        }
+
+    }
 }
