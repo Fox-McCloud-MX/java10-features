a12287cf1de9e8b1c82f0e32fa85325c98e4e543
seeand <kdsfoxy@gmail.com>
2020-05-28 00:44:11 -0500

    CopyOf new Feature

--- src/main/java/com/mx/java10/featrues/CopyOf.java
+++ src/main/java/com/mx/java10/featrues/CopyOf.java
@@ -1,4 +1,17 @@
 package com.mx.java10.featrues;
 
+import java.util.List;
+
+/*
+java.util.List, java.util.Map and java.util.Set each got a new static method copyOf(Collection).
+It returns the unmodifiable copy of the given Collection:
+*/
 public class CopyOf {
+
+    public static void main(String[] args) {
+        List someIntList = List.of(1,2,3,4,5);
+
+        List<Integer> copyList = List.copyOf(someIntList);
+        copyList.add(4); //java.lang.UnsupportedOperationException
+    }
 }
